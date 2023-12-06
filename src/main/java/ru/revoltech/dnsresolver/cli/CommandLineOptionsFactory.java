25a7faf5cd21566f6a287e9841605fc4c8188a97
ignusin <y.gnusin@yandex.ru>
2023-12-07 00:06:09 +0700

    Fixed tests.

--- src/main/java/ru/revoltech/dnsresolver/cli/CommandLineOptionsFactory.java
+++ src/main/java/ru/revoltech/dnsresolver/cli/CommandLineOptionsFactory.java
@@ -6,7 +6,10 @@ import org.apache.commons.cli.Options;
 class CommandLineOptionsFactory {
     Options buildOptions() {
         Option nameServerOption = Option.builder("ns").hasArg().type(String.class).build();
+        nameServerOption.setRequired(true);
+
         Option hostOption = Option.builder("h").hasArg().type(String.class).build();
+        hostOption.setRequired(true);
 
         Options options = new Options();
         options.addOption(nameServerOption);
